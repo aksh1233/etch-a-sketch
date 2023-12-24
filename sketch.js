ed56906e35b8e5bea26a8f6e146835bec2f88a66
Akshay Dubey <divine.akshay@gmail.com>
2023-12-24 15:59:56 +0530

    make the drawing pad dynamic
    done by adusting size of each pixel i.e the height and width

--- sketch.js
+++ sketch.js
@@ -1,19 +1,26 @@
-
-
 const gridContainer = document.querySelector('#gridContainer');
-for(let i=1;i<=16;i++) //this loop creates n flexboxes(div elements) arranged vertically
+createGrid(90);
+fillColor();
+
+function createGrid(n){
+for(let i=1;i<=n;i++) //this loop creates n flexboxes(div elements) arranged vertically
 {
     const nthContainer = document.createElement('div');
-    for(let j=1;j<=16;j++)// this loop creates n div elements inside each flexbox(div)
+    for(let j=1;j<=n;j++)// this loop creates n div elements inside each flexbox(div) and also makes the flexbox dynamic by adjusting the height and width of each pixel and the each flexbox
     {
         const pixel = document.createElement('div');
         pixel.classList.add('pix');
+        pixel.style.width = (800/n)+'px';
+        pixel.style.height = (800/n)+'px';
         nthContainer.appendChild(pixel);
     }
     nthContainer.style.display = 'flex';
+    nthContainer.style.width = '800px';
+    nthContainer.style.height = (800/n)+'px';
     gridContainer.appendChild(nthContainer);
-}
+}}
 
+function fillColor(){
 const drawingPixels = document.querySelectorAll('.pix');
 let mousedown = false;
 
@@ -22,10 +29,10 @@ for(let i=0;i<drawingPixels.length;i++)  //Handling the mouse events to fill the
     drawingPixels[i].addEventListener('mouseover',changeColor);
     drawingPixels[i].addEventListener('mousedown',changeMouseDownState);
     drawingPixels[i].addEventListener('mousedown',changeColor);
-    
+
     drawingPixels[i].addEventListener('mouseup',changeMouseUpState);
 
-   
+
 }
 function changeMouseUpState()
 {
@@ -42,4 +49,5 @@ function changeColor(e)
       return;
 
      e.target.style.backgroundColor = 'black';
+}
 }
